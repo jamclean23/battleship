5961d2125b6ab5c9f00b1f657d805f380a0a370d
jamclean23 <jamclean23@gmail.com>
2023-02-01 14:29:12 -0500

    Add rendering of game squares to boards

--- src/battleship/dom-manipulation.js
+++ src/battleship/dom-manipulation.js
@@ -1,32 +1,36 @@
 // Dom methods and rendering
 
 function initialize () {
+    // Render guides for each board
     const boards = document.querySelectorAll('.board');
-
-    addEmpty(boards[0]);
-    renderLetters(boards[0]);
-    renderNumbers(boards[0]);
+    const letters = [
+        'A', 'B', 'C', 'D', 'E', 'F',
+        'G', 'H', 'I', 'J'
+    ];
+    boards.forEach((board) => {
+        addEmpty(board);
+        renderLetters(board);
+        renderNumbers(board);
+        renderNodeSquares(board);
+    });
 
     function addEmpty (board) {
         // Create empty square and give it a class
         let newDiv = document.createElement('div');
         newDiv.classList.add('empty');
         newDiv.classList.add('gridSquare');
-
-
+        
         // Placement
-        newDiv.style.gridColumn = '0 / 1';
-        newDiv.style.gridRow = '0 / 1';
-
+        newDiv.style.backgroundColor = 'black';
+        newDiv.style.gridArea = '0/0/1/1';
+        
         // Add to DOM
         board.appendChild(newDiv);
+
+
     }
 
     function renderLetters (board) {
-        const letters = [
-            'A', 'B', 'C', 'D', 'E', 'F',
-            'G', 'H', 'I', 'J'
-        ];
 
         // Generate ten divs
         for (let i = 0; i < 10; i++) {
@@ -41,7 +45,7 @@ function initialize () {
 
 
             // Placement
-            newDiv.style.gridRow = '0 / 1';
+            newDiv.style.gridArea = '0/' + i + '/1/' + (i + 1);
 
             // Add to DOM
             board.appendChild(newDiv);
@@ -61,10 +65,36 @@ function initialize () {
             newDiv.classList.add('gridSquare');
 
             // Placement
-            newDiv.style.gridColumn = ' 1 / 2';
+            newDiv.style.gridColumn = '0 / 1';
+            newDiv.style.gridRow = (i + 2) + '/' + (i + 3);
 
             // Add to DOM
             board.appendChild(newDiv);
+        }
+    }
+
+    function renderNodeSquares(board) {
+        // create 100 divs, separated by ten to place them in rows
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+                let newDiv = document.createElement('div');
+
+                // Give it a class
+                newDiv.className = 'gridSquare gameSquare';
+
+                // Attach metadata about positoning
+                newDiv.meta = {
+                    x: j,
+                    y: i
+                }
+
+                // Placement
+                newDiv.style.gridColumn = (newDiv.meta.x + 2) + ' / ' +  (newDiv.meta.x + 3);
+                newDiv.style.gridRow = (newDiv.meta.y + 2) + ' / ' +  (newDiv.meta.y + 3);
+
+                // Add to DOM
+                board.appendChild(newDiv);
+            }
         }
     }
 }
